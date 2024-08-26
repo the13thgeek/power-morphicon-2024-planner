4d7a4f1fba919fbb217d23c6ffe30fd7c3356a03
the13thgeek <aldreich.aguilar@yahoo.com>
2024-08-26 11:53:50 -0400

    Added thank you message

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,14 @@ const App = () => {
       </header>
       <div className="content-box info">
         <h1>Power Morphicon 2024 Planner</h1>
+          <div className="thank-you">
+            <small>August 26, 2024</small>
+            <h3>That's a wrap!</h3>
+            <p>Thank you for the fun weekend, everyone! It's been a treat seeing old friends again after two years; and same goes for meeting new ones and creating new experiences with everyone.</p>
+            <p>And of course, thank you to everyone who used this page as a handy reference. I apologize not being able to update it in real-time during the event as much as I could; but I hope that this had been helpful in one way or another.</p>
+            <p>Until then, may the Power protect you always. ⚡</p>
+            <small>- the13thgeek</small>
+          </div>
         <p>The panel and photo-op schedules for this year's PMC are out! Use the text box below to search for a panel and any matching events will show up in the table below.</p>
         <p className="disclaimer"><b>Last Update:</b> Aug 21, 2024 8:41 PM EST</p>
         <p className="disclaimer"><b>Disclaimer:</b> The data displayed on this page is sourced directly from <a href="//officialpowermorphicon.com" target="_blank">Power Morphicon</a>. We do not verify the accuracy of the information provided by PMC. Therefore, we are not responsible for any errors, inaccuracies, or discrepancies in the data. The information is provided "as is," and we recommend verifying details directly with PMC for the most accurate and up-to-date information.</p>
