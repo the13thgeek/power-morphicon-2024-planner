be4f980bf1bc36b27ea924b04021f460897e4576
the13thgeek <aldreich.aguilar@yahoo.com>
2024-09-09 12:02:15 -0400

    Removed 2024 references

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ const App = () => {
     <Router>
       <Routes>
         <Route index element={<PageHome />} />
-        <Route exact path="/power-morphicon-2024-planner" element={<PageHome />} />
-        <Route exact path="/power-morphicon-2024-planner/panels" element={<PagePanels />} />
-        <Route exact path="/power-morphicon-2024-planner/photo-ops" element={<PagePhotoOps />} />
+        <Route exact path="/power-morphicon-planner" element={<PageHome />} />
+        <Route exact path="/power-morphicon-planner/panels" element={<PagePanels />} />
+        <Route exact path="/power-morphicon-planner/photo-ops" element={<PagePhotoOps />} />
       </Routes>
     </Router>
 
